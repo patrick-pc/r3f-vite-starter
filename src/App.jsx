3f141ef7e4c5361934d8341f8cd5bf29dfc9eb0a
spidΞy <web3slinger@gmail.com>
2023-08-03 21:09:44 -0500

    basic scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Suspense } from "react";
+import { Physics } from "@react-three/rapier";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
+    <Canvas shadows camera={{ position: [0, 5, 5], fov: 100 }}>
+      <color attach="background" args={["#e3daf7"]} />
+      <fog attach="fog" args={["#dbecfb", 30, 40]} />
+      <Suspense>
+        <Physics debug>
+          <Experience />
+        </Physics>
+      </Suspense>
     </Canvas>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,60 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Cylinder,
+  MeshReflectorMaterial,
+  OrbitControls,
+} from "@react-three/drei";
+import { RigidBody, CylinderCollider } from "@react-three/rapier";
+import { Torii } from "./Torii";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      {/*  Lights */}
+      <ambientLight intensity={1} />
+      <directionalLight
+        position={[5, 5, 5]}
+        intensity={0.8}
+        castShadow
+        color={"#9e69da"}
+      />
+
+      {/* Background */}
+
+      <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[50, 50]} />
+        <MeshReflectorMaterial
+          blur={[400, 200]}
+          resolution={1024}
+          mixBlur={1}
+          mixStrength={15}
+          depthScale={1}
+          minDepthThreshold={0.85}
+          color="#dbecfb"
+        />
       </mesh>
+
+      <Torii
+        scale={[16, 16, 16]}
+        position={[0, 0, -22]}
+        rotation-y={1.25 * Math.PI}
+      />
+      <Torii
+        scale={[10, 10, 10]}
+        position={[-8, 0, -20]}
+        rotation-y={1.4 * Math.PI}
+      />
+      <Torii scale={[10, 10, 10]} position={[8, 0, -20]} rotation-y={Math.PI} />
+
+      {/* Stage */}
+      <group position-y={-1}>
+        <RigidBody colliders={false} type="fixed" position-y={[-0.5]}>
+          <CylinderCollider args={[1 / 2, 5]} />
+          <Cylinder scale={[5, 1, 5]} receiveShadow>
+            <meshStandardMaterial color="white" />
+          </Cylinder>
+        </RigidBody>
+      </group>
     </>
   );
 };
